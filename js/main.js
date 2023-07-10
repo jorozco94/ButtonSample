e74dc11d3f271768ce19ee34f42ceb17dfaf48ea
jorozco94 <91165810+jorozco94@users.noreply.github.com>
2023-07-09 22:52:04 -0700

    chore: Folder structure

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// Add a button to the 3 section elements
+// grab each section element
+// add a button to the section element
+import { buttonCreator } from "../js/buttonHelper.js";
+const sections = document.querySelectorAll('section');
+
+// const buttonCreator = (element) => {
+//   const btn = document.createElement('button');
+//   btn.textContent = 'Click Here'
+//   element.appendChild(btn);
+// }
+
+sections.forEach((section) => buttonCreator(section, {color: 'blue', center: true}));
